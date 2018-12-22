3b8fc53b52056bd35abee4aaece5ed66f5c22313
Josh Reddick <josh.reddick@coxautoinc.com>
2018-12-22 14:41:10 -0500

    added shape and color tutorials

--- ball_tracking.py
+++ ball_tracking.py
@@ -20,8 +20,8 @@ args = vars(ap.parse_args())
 # list of tracked points
 # greenLower = (29, 86, 6)
 # greenUpper = (64, 255, 255)
-yellowLower = (20, 100, 100)
-yellowUpper = (30, 255, 255)
+yellowLower = (25, 125, 125)
+yellowUpper = (35, 255, 255)
 pts = deque(maxlen=args["buffer"])
 
 # if a video path was not supplied, grab the reference
@@ -71,36 +71,36 @@ while True:
 
 	# only proceed if at least one contour was found
 	if len(cnts) > 0:
-		# find the largest contour in the mask, then use
+		# for ever contour in the mask, use
 		# it to compute the minimum enclosing circle and
 		# centroid
-		c = max(cnts, key=cv2.contourArea)
-		((x, y), radius) = cv2.minEnclosingCircle(c)
-		M = cv2.moments(c)
-		center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
+		for contour in cnts:
+			((x, y), radius) = cv2.minEnclosingCircle(contour)	
+			M = cv2.moments(contour)
+			center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
 
-		# only proceed if the radius meets a minimum size
-		if radius > 10:
-			# draw the circle and centroid on the frame,
-			# then update the list of tracked points
-			cv2.circle(frame, (int(x), int(y)), int(radius),
-				(0, 255, 255), 2)
-			cv2.circle(frame, center, 5, (0, 0, 255), -1)
+			# only proceed if the radius meets a minimum size
+			if radius > 10:
+				# draw the circle and centroid on the frame,
+				# then update the list of tracked points
+				cv2.circle(frame, (int(x), int(y)), int(radius),
+					(0, 255, 255), 2)
+				cv2.circle(frame, center, 5, (0, 0, 255), -1)
 
-	# update the points queue
-	pts.appendleft(center)
+			# update the points queue
+			pts.appendleft(center)
 
 	# loop over the set of tracked points
-	for i in range(1, len(pts)):
-		# if either of the tracked points are None, ignore
-		# them
-		if pts[i - 1] is None or pts[i] is None:
-			continue
+	# for i in range(1, len(pts)):
+	# 	# if either of the tracked points are None, ignore
+	# 	# them
+	# 	if pts[i - 1] is None or pts[i] is None:
+	# 		continue
 
-		# otherwise, compute the thickness of the line and
-		# draw the connecting lines
-		thickness = int(np.sqrt(args["buffer"] / float(i + 1)) * 2.5)
-		cv2.line(frame, pts[i - 1], pts[i], (0, 0, 255), thickness)
+	# 	# otherwise, compute the thickness of the line and
+	# 	# draw the connecting lines
+	# 	thickness = int(np.sqrt(args["buffer"] / float(i + 1)) * 2.5)
+	# 	cv2.line(frame, pts[i - 1], pts[i], (0, 0, 255), thickness)
 
 	# show the frame to our screen
 	cv2.imshow("Frame", frame)
